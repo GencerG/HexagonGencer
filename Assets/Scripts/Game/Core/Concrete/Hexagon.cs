73d7ee2d2b2d7ba37d2ca2cc908aa39770c8c943
GencerG <gencer.guzeloglu@outlook.com>
2021-07-15 14:57:16 +0300

    feat: add Hexagon class

---
+++ Assets/Scripts/Game/Core/Concrete/Hexagon.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace HexagonGencer.Game.Core.Concrete
+{
+    public class Hexagon : MonoBehaviour
+    {
+        #region Fields
+
+        #endregion
+
+        #region Unity
+
+        #endregion
+
+        #region Custom Methods
+
+
+
+        #endregion
+    }
+}
