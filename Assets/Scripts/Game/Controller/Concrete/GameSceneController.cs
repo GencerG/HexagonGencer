72fb6bd7016dd29772b6bd12689a97520b1adecc
GencerG <gencer.guzeloglu@outlook.com>
2021-07-15 15:00:43 +0300

    feat: initialize pool

--- Assets/Scripts/Game/Controller/Concrete/GameSceneController.cs
+++ Assets/Scripts/Game/Controller/Concrete/GameSceneController.cs
@@ -1,4 +1,7 @@
+using HexagonGencer.Factory;
 using HexagonGencer.Game.Controller.Abstract;
+using HexagonGencer.Game.Models.Concrete;
+using HexagonGencer.Utils;
 using UniRx;
 using UnityEngine;
 
@@ -6,6 +9,15 @@ namespace HexagonGencer.Game.Controller.Concrete
 {
     public partial class GameSceneController : SceneController
     {
+        #region Fields
+
+        private ObjectPool _objectPool;
+
+        private GameObject _poolContainer;
+        private GameObject _outline;
+
+        #endregion
+
         #region Scene Controller
 
         public override BoolReactiveProperty ShouldRenderNewScene { get; set; }
@@ -14,6 +26,20 @@ namespace HexagonGencer.Game.Controller.Concrete
         public override void InitializeScene()
         {
 
+        }
+
+        #endregion
+
+        #region Custom Methods
+
+        private void InitializePool()
+        {
+            var prefab = AssetFactory
+                .GetAsset(GameObjectAssetModel.HexagonPrefab);
+
+            _poolContainer = GameObject.Find("PoolContainer");
+            _objectPool = new ObjectPool(prefab, _poolContainer.transform);
+            _objectPool.InstantiatePool();
         }
 
         #endregion
