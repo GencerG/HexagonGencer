3a019ad2d4712e584830f2a6d7c197539f72a0f2
GencerG <gencer.guzeloglu@outlook.com>
2021-07-15 17:58:33 +0300

    feat: add new helper methods

--- Assets/Scripts/Utils/HexagonGencerUtils.cs
+++ Assets/Scripts/Utils/HexagonGencerUtils.cs
@@ -1,4 +1,6 @@
 using HexagonGencer.Enums;
+using HexagonGencer.Game.Core.Concrete;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HexagonGencer.Utils
@@ -9,6 +11,10 @@ namespace HexagonGencer.Utils
 
         public static int BOARD_WIDTH = 8;
         public static int BOARD_HEIGHT = 9;
+        public static int NUMBER_OF_COLORS = 7;
+        public static readonly float CLOCK_WISE = -120f;
+        public static readonly float COUNTER_CLOCK_WISE = 120f;
+        public const float ROTATION_ANIMATON_DURATION = .2f;
 
         public const float x_MUL = 1.9f;
         public const float y_MUL = 2.2f;
@@ -20,7 +26,44 @@ namespace HexagonGencer.Utils
 
         #region Helper Methods
 
-        public static Vector2 GetHexagonPosition(int i, int j)
+        public static void MoveItemInList<T>(this List<T> list, int oldIndex, int newIndex)
+        {
+            var item = list[oldIndex];
+            list[oldIndex] = list[newIndex];
+            list[newIndex] = item;
+        }
+
+        public static Cell[] GetNeighboursFromCell(Cell cell, List<Cell> cellList)
+        {
+            var remainder = cell.Column % 2;
+            var offset = BOARD_WIDTH * remainder;
+
+            var neighbours = new Cell[6] {
+                cell.Index < cellList.Count - BOARD_WIDTH ? cellList[cell.Index + BOARD_WIDTH] : null,
+                cell.Index < cellList.Count - (BOARD_WIDTH + 1) + offset ? cellList[cell.Index + (BOARD_WIDTH + 1) - offset] : null,
+                cell.Index >= -1 + offset && cell.Index != cellList.Count - 1 ? cellList[cell.Index + 1 - offset] : null,
+                cell.Index >= BOARD_WIDTH ? cellList[cell.Index - BOARD_WIDTH] : null,
+                cell.Index >= 1 + offset ? cellList[cell.Index - 1 - offset] : null,
+                cell.Index < cellList.Count - (BOARD_WIDTH - 1) + offset ? cellList[cell.Index + (BOARD_WIDTH - 1) - offset] : null,
+            };
+
+
+            if (cell.Column == 0)
+            {
+                neighbours[4] = null;
+                neighbours[5] = null;
+            }
+
+            if (cell.Column == BOARD_WIDTH - 1)
+            {
+                neighbours[1] = null;
+                neighbours[2] = null;
+            }
+
+            return neighbours;
+        }
+
+        public static Vector2 GetItemPosition(int i, int j)
         {
             if (j % 2 == 0)
             {
@@ -41,7 +84,7 @@ namespace HexagonGencer.Utils
             return hit;
         }
 
-        public static HexagonCorner GetHexCorner(Vector2 hexPosition, Vector2 hitPosition)
+        public static ItemCorner GetHexCorner(Vector2 hexPosition, Vector2 hitPosition)
         {
             var direction = hitPosition - hexPosition;
             direction.Normalize();
@@ -50,37 +93,37 @@ namespace HexagonGencer.Utils
 
             if (angle > -30f && angle <= 30f)
             {
-                return HexagonCorner.Right;
+                return ItemCorner.Right;
             }
 
             else if (angle > 30f && angle <= 90f)
             {
-                return HexagonCorner.TopRight;
+                return ItemCorner.TopRight;
             }
 
             else if (angle > 90f && angle <= 150f)
             {
-                return HexagonCorner.TopLeft;
+                return ItemCorner.TopLeft;
             }
 
             else if (angle > 150f && angle <= -150f)
             {
-                return HexagonCorner.Left;
+                return ItemCorner.Left;
             }
 
             else if (angle > -150f && angle <= -90f)
             {
-                return HexagonCorner.BottomLeft;
+                return ItemCorner.BottomLeft;
             }
 
             else if (angle > -90f && angle <= -30f)
             {
-                return HexagonCorner.BottomRight;
+                return ItemCorner.BottomRight;
             }
 
             else
             {
-                return HexagonCorner.Left;
+                return ItemCorner.Left;
             }
         }
 
