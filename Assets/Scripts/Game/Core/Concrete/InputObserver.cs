d295d9c06ee773051d0dc9a96dd33741b3676de0
GencerG <gencer.guzeloglu@outlook.com>
2021-07-15 12:38:46 +0300

    refactor: add namespace and regions

--- Assets/Scripts/Game/Core/Concrete/InputObserver.cs
+++ Assets/Scripts/Game/Core/Concrete/InputObserver.cs
@@ -3,159 +3,174 @@ using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 
-public class InputObserver : ObservableEventTrigger
+namespace HexagonGencer.Game.Core.Concrete
 {
-    [SerializeField] private float _minTime = 1;
-    [SerializeField] private float _minDistance = 100.0f;
-
-    private Vector2 _startPosition;
-    private DateTime _startTime;
-
-    private CompositeDisposable _disposables = new CompositeDisposable();
-
-    private Subject<Unit> _onSwipeLeft = new Subject<Unit>();
-    public IObservable<Unit> OnSwipeLeft
+    public class InputObserver : ObservableEventTrigger
     {
-        get { return _onSwipeLeft; }
-    }
+        #region Fields
 
-    private Subject<Unit> _onSwipeRight = new Subject<Unit>();
-    public IObservable<Unit> OnSwipeRight
-    {
-        get { return _onSwipeRight; }
-    }
+        [SerializeField] private float _minTime = 1;
+        [SerializeField] private float _minDistance = 100.0f;
 
-    private Subject<Unit> _onSwipeDown = new Subject<Unit>();
-    public IObservable<Unit> OnSwipeDown
-    {
-        get { return _onSwipeDown; }
-    }
+        private Vector2 _startPosition;
+        private DateTime _startTime;
 
-    private Subject<Unit> _onSwipeUp = new Subject<Unit>();
-    public IObservable<Unit> OnSwipeUp
-    {
-        get { return _onSwipeUp; }
-    }
+        private CompositeDisposable _disposables = new CompositeDisposable();
 
-    private Subject<Vector2> _onClick = new Subject<Vector2>();
-    public IObservable<Vector2> OnClick
-    {
-        get { return _onClick; }
-    }
+        #endregion
 
-    private void OnEnable()
-    {
-        var beginDragStream = this
-            .OnBeginDragAsObservable()
-            .TakeUntilDisable(this)
-            .Select(eventData => eventData.position)
-            .Subscribe(position =>
-            {
-                this._startPosition = position;
-                this._startTime = DateTime.Now;
+        #region Subjects
 
-            }).AddTo(_disposables);
+        private Subject<Unit> _onSwipeLeft = new Subject<Unit>();
+        public IObservable<Unit> OnSwipeLeft
+        {
+            get { return _onSwipeLeft; }
+        }
 
-        var endDragStream = this
-            .OnEndDragAsObservable()
-            .TakeUntilDisable(this)
-            .Where(_ => (DateTime.Now - _startTime).TotalSeconds < _minTime)
-            .Select(eventData => eventData.position)
-            .Share();
+        private Subject<Unit> _onSwipeRight = new Subject<Unit>();
+        public IObservable<Unit> OnSwipeRight
+        {
+            get { return _onSwipeRight; }
+        }
 
-        var pointerDownStream = this
-            .OnPointerDownAsObservable()
-            .TakeUntilDisable(this)
-            .Select(eventData => eventData.position)
-            .Subscribe(position =>
-            {
+        private Subject<Unit> _onSwipeDown = new Subject<Unit>();
+        public IObservable<Unit> OnSwipeDown
+        {
+            get { return _onSwipeDown; }
+        }
 
-                this._startPosition = position;
+        private Subject<Unit> _onSwipeUp = new Subject<Unit>();
+        public IObservable<Unit> OnSwipeUp
+        {
+            get { return _onSwipeUp; }
+        }
 
-            }).AddTo(_disposables);
+        private Subject<Vector2> _onClick = new Subject<Vector2>();
+        public IObservable<Vector2> OnClick
+        {
+            get { return _onClick; }
+        }
 
-        var pointerUpStream = this
-            .OnPointerUpAsObservable()
-            .TakeUntilDisable(this)
-            .Select(eventData => eventData.position)
-            .Share();
+        #endregion
 
-        endDragStream
-            .Where(position =>
-            {
-                var diffrence = (position - _startPosition).normalized;
-                return diffrence.y > -0.5 && diffrence.y < 0.5;
-            })
-            .Where(position => _startPosition.x > position.x)
-            .Where(position => Mathf.Abs(_startPosition.x - position.x) >= _minDistance)
-            .Subscribe(_ =>
-            {
-                Debug.Log("Swipe Left");
-                _onSwipeLeft.OnNext(Unit.Default);
+        #region Unity
 
-            }).AddTo(_disposables);
+        private void OnEnable()
+        {
+            var beginDragStream = this
+                .OnBeginDragAsObservable()
+                .TakeUntilDisable(this)
+                .Select(eventData => eventData.position)
+                .Subscribe(position =>
+                {
+                    this._startPosition = position;
+                    this._startTime = DateTime.Now;
 
-        endDragStream
-            .Where(position =>
-            {
-                var diffrence = (position - _startPosition).normalized;
-                return diffrence.y > -0.5 && diffrence.y < 0.5;
-            })
-            .Where(position => position.x > _startPosition.x)
-            .Where(position => Mathf.Abs(position.x - _startPosition.x) >= _minDistance)
-            .Subscribe(_ =>
-            {
-                Debug.Log("Swipe Right");
-                _onSwipeRight.OnNext(Unit.Default);
+                }).AddTo(_disposables);
 
-            }).AddTo(_disposables);
+            var endDragStream = this
+                .OnEndDragAsObservable()
+                .TakeUntilDisable(this)
+                .Where(_ => (DateTime.Now - _startTime).TotalSeconds < _minTime)
+                .Select(eventData => eventData.position)
+                .Share();
 
-        endDragStream
-            .Where(position =>
-            {
-                var diffrence = (position - _startPosition).normalized;
-                return diffrence.x > -0.5 && diffrence.x < 0.5;
-            })
-            .Where(position => _startPosition.y > position.y)
-            .Where(position => Mathf.Abs(_startPosition.y - position.y) >= _minDistance)
-            .Subscribe(_ =>
-            {
-                Debug.Log("Swipe Down");
-                _onSwipeDown.OnNext(Unit.Default);
+            var pointerDownStream = this
+                .OnPointerDownAsObservable()
+                .TakeUntilDisable(this)
+                .Select(eventData => eventData.position)
+                .Subscribe(position =>
+                {
 
-            }).AddTo(_disposables);
+                    this._startPosition = position;
 
-        endDragStream
-            .Where(position =>
-            {
-                var diffrence = (position - _startPosition).normalized;
-                return diffrence.x > -0.5 && diffrence.x < 0.5;
-            })
-            .Where(position => position.y > _startPosition.y)
-            .Where(position => Mathf.Abs(position.y - _startPosition.y) >= _minDistance)
-            .Subscribe(_ =>
-            {
-                Debug.Log("Swipe Up");
-                _onSwipeUp.OnNext(Unit.Default);
+                }).AddTo(_disposables);
 
-            }).AddTo(_disposables);
+            var pointerUpStream = this
+                .OnPointerUpAsObservable()
+                .TakeUntilDisable(this)
+                .Select(eventData => eventData.position)
+                .Share();
 
-        pointerUpStream
-            .Where(position =>
-            {
-                var diffrence = (position - _startPosition).magnitude;
-                return diffrence < 10f;
-            })
-            .Subscribe(position =>
-            {
-                Debug.Log("Click");
-                _onClick.OnNext(position);
+            endDragStream
+                .Where(position =>
+                {
+                    var diffrence = (position - _startPosition).normalized;
+                    return diffrence.y > -0.5 && diffrence.y < 0.5;
+                })
+                .Where(position => _startPosition.x > position.x)
+                .Where(position => Mathf.Abs(_startPosition.x - position.x) >= _minDistance)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Swipe Left");
+                    _onSwipeLeft.OnNext(Unit.Default);
 
-            }).AddTo(_disposables);
-    }
+                }).AddTo(_disposables);
 
-    private void OnDisable()
-    {
-        _disposables.Dispose();
+            endDragStream
+                .Where(position =>
+                {
+                    var diffrence = (position - _startPosition).normalized;
+                    return diffrence.y > -0.5 && diffrence.y < 0.5;
+                })
+                .Where(position => position.x > _startPosition.x)
+                .Where(position => Mathf.Abs(position.x - _startPosition.x) >= _minDistance)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Swipe Right");
+                    _onSwipeRight.OnNext(Unit.Default);
+
+                }).AddTo(_disposables);
+
+            endDragStream
+                .Where(position =>
+                {
+                    var diffrence = (position - _startPosition).normalized;
+                    return diffrence.x > -0.5 && diffrence.x < 0.5;
+                })
+                .Where(position => _startPosition.y > position.y)
+                .Where(position => Mathf.Abs(_startPosition.y - position.y) >= _minDistance)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Swipe Down");
+                    _onSwipeDown.OnNext(Unit.Default);
+
+                }).AddTo(_disposables);
+
+            endDragStream
+                .Where(position =>
+                {
+                    var diffrence = (position - _startPosition).normalized;
+                    return diffrence.x > -0.5 && diffrence.x < 0.5;
+                })
+                .Where(position => position.y > _startPosition.y)
+                .Where(position => Mathf.Abs(position.y - _startPosition.y) >= _minDistance)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Swipe Up");
+                    _onSwipeUp.OnNext(Unit.Default);
+
+                }).AddTo(_disposables);
+
+            pointerUpStream
+                .Where(position =>
+                {
+                    var diffrence = (position - _startPosition).magnitude;
+                    return diffrence < 10f;
+                })
+                .Subscribe(position =>
+                {
+                    Debug.Log("Click");
+                    _onClick.OnNext(position);
+
+                }).AddTo(_disposables);
+        }
+
+        #endregion
+
+        private void OnDisable()
+        {
+            _disposables.Dispose();
+        }
     }
 }
