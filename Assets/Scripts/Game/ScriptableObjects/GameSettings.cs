98c75d3975f7b520124d9f1ab5be91694fb9713f
GencerG <gencer.guzeloglu@outlook.com>
2021-07-16 11:27:06 +0300

    refactor: add setter

--- Assets/Scripts/Game/ScriptableObjects/GameSettings.cs
+++ Assets/Scripts/Game/ScriptableObjects/GameSettings.cs
@@ -9,8 +9,8 @@ namespace HexagonGencer.Game.Scripables
         [SerializeField] private int _boardHeight;
         [SerializeField] private int _numberOfColors;
 
-        public int BOARD_WIDTH{ get { return _boardWidth; } set { } }
-        public int BOARD_HEIGHT { get { return _boardHeight; } set { } }
-        public int NUMBER_OF_COLORS { get { return _numberOfColors; } set { } }
+        public int BOARD_WIDTH{ get { return _boardWidth; } set { _boardWidth = value; } }
+        public int BOARD_HEIGHT { get { return _boardHeight; } set { _boardHeight = value; } }
+        public int NUMBER_OF_COLORS { get { return _numberOfColors; } set { _numberOfColors = value; } }
     }
 }
