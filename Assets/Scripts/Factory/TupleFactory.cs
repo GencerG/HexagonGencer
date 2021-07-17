90c1ca55785a02e7d10a17bb3d4309baccde3c69
Gencer Güzeloğlu <59722791+GencerG@users.noreply.github.com>
2021-07-17 21:55:10 +0300

    refactor: change tuple type from IItem to item

--- Assets/Scripts/Factory/TupleFactory.cs
+++ Assets/Scripts/Factory/TupleFactory.cs
@@ -1,19 +1,19 @@
-using HexagonGencer.Game.Models.Abstract;
+using HexagonGencer.Game.Core.Abstract;
 using System;
 
 namespace HexagonGencer.Factory
 {
     public static class TupleFactory
     {
-        public static Tuple<IItem, IItem, IItem> GetItemTuple(IItem firstPiece, int corner)
+        public static Tuple<Item, Item, Item> GetItemTuple(Item firstPiece, int corner)
         {
             var neighbours = firstPiece.Cell.GetNeighbours();
 
             if (neighbours[corner] == null || neighbours[(corner + 1) % 6] == null)
                 return null;
 
-            IItem secondPiece = neighbours[corner].Item;
-            IItem thirdPiece = neighbours[(corner + 1) % 6].Item;
+            Item secondPiece = neighbours[corner].Item;
+            Item thirdPiece = neighbours[(corner + 1) % 6].Item;
 
             return Tuple.Create(firstPiece, secondPiece, thirdPiece);
         }
